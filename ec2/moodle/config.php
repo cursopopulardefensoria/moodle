66a31ceb42bc3610eb85958c665c26ae82b42abf
Rafael Santos <rafael.santos.193@gmail.com>
2018-07-29 18:52:14 -0300

    feat: Add no-reply settings

--- ec2/moodle/config.php
+++ ec2/moodle/config.php
@@ -4,13 +4,13 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
+/* Database settings */
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = getenv('RDS_HOSTNAME');
 $CFG->dbname    = getenv('RDS_DB_NAME');
 $CFG->dbuser    = getenv('RDS_USERNAME');
 $CFG->dbpass    = getenv('RDS_PASSWORD');
-
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
@@ -18,6 +18,17 @@ $CFG->dboptions = array (
   'dbsocket' => '',
 );
 
+/* E-mail Settings */
+$CFG->debugsmtp         = getenv("SMTP_DEBUG_SMTP");
+$CFG->smtphosts         = getenv("SMTP_HOSTS");
+$CFG->smtpsecure        = getenv("SMTP_SECURE");
+$CFG->smtpuser          = getenv("SMTP_USER");
+$CFG->smtppass          = getenv("SMTP_PASSWORD");
+$CFG->divertallemailsto = getenv("SMTP_DIVERT_ALL_EMAILS_TO");
+$CFG->noreplyaddress    = getenv("SMTP_NO_REPLY_ADDRESS");
+$CFG->emailonlyfromnoreplyaddress = getenv("SMTP_EMAIL_ONLY_FROM_NO_REPLY_ADDRESS");
+
+/* Moodle setup */
 $CFG->wwwroot   = getenv('MOODLE_WWW_ROOT');
 $CFG->dataroot  = getenv('MOODLE_DATA_ROOT');
 $CFG->sslproxy  = getenv('MOODLE_SSL_PROXY');
@@ -25,14 +36,6 @@ $CFG->admin     = 'admin';
 $CFG->tempdir = "/tmp";
 $CFG->localcache = "/tmp";
 $CFG->cachedir = "/tmp";
-
-/* SMTP Settings */
-$CFG->debugsmtp         = getenv("SMTP_DEBUG_SMTP");
-$CFG->smtphosts         = getenv("SMTP_HOSTS");
-$CFG->smtpsecure        = getenv("SMTP_SECURE");
-$CFG->smtpuser          = getenv("SMTP_USER");
-$CFG->smtppass          = getenv("SMTP_PASSWORD");
-$CFG->divertallemailsto = getenv("SMTP_DIVERT_ALL_EMAILS_TO");
 
 $CFG->directorypermissions = 0777;
 
